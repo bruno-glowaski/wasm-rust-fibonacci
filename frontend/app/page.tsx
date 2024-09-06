37ea2b4bf396174c45a265ba5b6fabac77e11592
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2024-09-06 01:09:10 -0400

    fix: layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -41,6 +41,34 @@ export default function Home() {
     <>
       <h1 className="text-4xl text-center">Fibonacci Generator</h1>
       <main className="flex flex-col items-stretch gap-8">
+        <form
+          className="border border-slate-700 rounded p-8 flex flex-col gap-4"
+          onSubmit={generate}
+        >
+          <label className="flex flex-col gap-1">
+            <span className="text-xs">Sequence length:</span>
+            <input
+              className="border border-slate-800 rounded-md p-2 bg-slate-50 text-slate-900"
+              name="length"
+              min="1"
+              type="number"
+            />
+          </label>
+          <button
+            disabled={status === "working"}
+            className="border border-green-500 rounded p-2 bg-green-800 hover:bg-green-700 active:bg-green-600 disabled:bg-green-950 disabled:border-green-700"
+          >
+            Generate
+          </button>
+          <button
+            disabled={status !== "working"}
+            type="button"
+            className="border border-red-500 rounded p-2 bg-red-800 hover:bg-red-700 active:bg-red-600 disabled:bg-red-950 disabled:border-red-700"
+            onClick={cancelTask}
+          >
+            Cancel
+          </button>
+        </form>
         <figure className="border border-slate-700 rounded p-8 flex flex-row flex-wrap gap-4">
           {status === "idle" && (
             <figcaption className="flex-1 text-white text-opacity-50 h-16 flex justify-center items-center">
@@ -66,33 +94,6 @@ export default function Home() {
             </figcaption>
           )}
         </figure>
-        <form
-          className="border border-slate-700 rounded p-8 flex flex-col gap-4"
-          onSubmit={generate}
-        >
-          <label className="flex flex-col gap-1">
-            <span className="text-xs">Sequence length:</span>
-            <input
-              className="border border-slate-800 rounded-md p-2 bg-slate-50 text-slate-900"
-              name="length"
-              type="number"
-            />
-          </label>
-          <button
-            disabled={status === "working"}
-            className="border border-green-500 rounded p-2 bg-green-800 hover:bg-green-700 active:bg-green-600 disabled:bg-green-950 disabled:border-green-700"
-          >
-            Generate
-          </button>
-          <button
-            disabled={status !== "working"}
-            type="button"
-            className="border border-red-500 rounded p-2 bg-red-800 hover:bg-red-700 active:bg-red-600 disabled:bg-red-950 disabled:border-red-700"
-            onClick={cancelTask}
-          >
-            Cancel
-          </button>
-        </form>
       </main>
     </>
   );
