749be7bcd49df087d6ae1a63d32ab672dee83f3b
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2024-09-06 01:09:10 -0400

    feat: memoised algorithm

--- frontend/lib/fibonacci.bench.ts
+++ frontend/lib/fibonacci.bench.ts
@@ -1,12 +1,20 @@
 import { benchmarkSuite } from "jest-bench";
 
-import { fibonacci } from "./fibonacci";
+import { fibonacci, fibonacciNaive } from "./fibonacci";
 
 benchmarkSuite("fibonacci strategies", {
   ["Naive (10 items)"]() {
-    Array.from({ length: 10 }, (_, i) => fibonacci(i));
+    Array.from({ length: 10 }, (_, i) => fibonacciNaive(i));
   },
-  ["Naive (50 items)"]() {
-    Array.from({ length: 50 }, (_, i) => fibonacci(i));
+  ["Memoised (10 items)"]() {
+    const memo = {};
+    Array.from({ length: 10 }, (_, i) => fibonacci(i, memo));
+  },
+  ["Naive (100 items)"]() {
+    Array.from({ length: 100 }, (_, i) => fibonacciNaive(i));
+  },
+  ["Memoised (100 items)"]() {
+    const memo = {};
+    Array.from({ length: 100 }, (_, i) => fibonacci(i, memo));
   },
 });
--- frontend/lib/fibonacci.ts
+++ frontend/lib/fibonacci.ts
@@ -1,5 +1,20 @@
-export function fibonacci(index: number): number {
+export function fibonacciNaive(index: number): number {
   if (index <= 0) return 0;
   if (index <= 2) return 1;
-  return fibonacci(index - 1) + fibonacci(index - 2);
+  return fibonacciNaive(index - 1) + fibonacciNaive(index - 2);
 }
+
+export function fibonacciMemoised(
+  index: number,
+  memo: Record<number, number> = {},
+): number {
+  if (memo[index] != null) return memo[index];
+  if (index === 0) return 0;
+  if (index < 3) return 1;
+  const r = fibonacciMemoised(index - 1, memo) +
+    fibonacciMemoised(index - 2, memo);
+  memo[index] = r;
+  return r;
+}
+
+export const fibonacci = fibonacciMemoised;
