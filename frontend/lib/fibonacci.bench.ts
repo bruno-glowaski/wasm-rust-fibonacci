a6bb2fd7f3201b96c333880961dd7eb6f9e398d7
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2024-09-06 01:09:10 -0400

    refactor: benchmark suite

--- frontend/lib/fibonacci.bench.ts
+++ frontend/lib/fibonacci.bench.ts
@@ -2,11 +2,15 @@ import { benchmarkSuite } from "jest-bench";
 
 import { fibonacci } from "./fibonacci";
 
+function createSuiteSet(itemCount: number): Record<string, () => void> {
+  return {
+    [`JavaScript (${itemCount}) items`]() {
+      fibonacci(itemCount);
+    },
+  };
+}
+
 benchmarkSuite("fibonacci strategies", {
-  ["JavaScript (100 items)"]() {
-    fibonacci(100);
-  },
-  ["JavaScript (1000 items)"]() {
-    fibonacci(1000);
-  },
+  ...createSuiteSet(1000),
+  ...createSuiteSet(10000),
 });
